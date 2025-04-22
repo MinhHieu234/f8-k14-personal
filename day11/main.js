b468fb902f487470aa142efdc258a093ee67bf9c
MinhHieu234 <hieuz5kendark@gmail.com>
2025-04-22 12:40:16 +0700

    btvn 11

---
+++ day11/main.js
@@ -0,0 +1,53 @@
+// bai1
+// bieu do ben trang index
+
+// let a = 130;
+// let x = 0;
+// if (a <= 1) {
+//     x = 15000 * a;
+// } else if (a <= 5) {
+//     x = 15000 + (a - 1) * 13500;
+// } else if (a <= 120) {
+//     x = 15000 + 13500 * 4 + (a - 5) * 11000;
+// } else {
+//     x = (15000 + 13500 * 4 + (a - 5) * 11000) * 0.9;
+// }
+// console.log(`Cước phí cho ${a} km là: ${x.toLocaleString()} VND`);
+//  bai3
+
+// let n = 5;
+// let S = 0;
+//
+// for (let i = 1; i <= n; i++) {
+//     S += i * (i + 1);
+// }
+//
+// console.log("Giá trị của biểu thức S là: " + S);
+// bai4
+// let m = 10;
+//
+// if (m < 2) {
+//     console.log(m + " không phải là số nguyên tố");
+// } else {
+//     let laSoNguyenTo = true;
+//     for (let i = 2; i <= Math.sqrt(m); i++) {
+//         if (m % i === 0) {
+//             console.log(m + " không phải là số nguyên tố");
+//             laSoNguyenTo = false;
+//             break;
+//         }
+//     }
+//     if (laSoNguyenTo) {
+//         console.log(m + " là số nguyên tố");
+//     }
+// }
+// bai5
+// for (let i = 1; i <= 10; i++) {
+//     console.log("Bảng cửu chương " + i + ":");
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(i + " x " + j + " = " + (i * j));
+//     }
+//     console.log("");
+// }
+
+
